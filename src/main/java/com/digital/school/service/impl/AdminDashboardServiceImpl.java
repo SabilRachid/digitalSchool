2e42ffcd292781985f6650cc501ee9a5d371644e
rsabil <sabil.rachid@gmail.com>
2025-01-30 23:00:52 +0100

    extends entity student, parent, professor

--- src/main/java/com/digital/school/service/impl/AdminDashboardServiceImpl.java
+++ src/main/java/com/digital/school/service/impl/AdminDashboardServiceImpl.java
@@ -1,9 +1,20 @@
 package com.digital.school.service.impl;
 
+import com.digital.school.repository.SubjectRepository;
 import com.digital.school.service.AdminDashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class AdminDashboardServiceImpl implements AdminDashboardService {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
     @Override
     public List<Map<String, Object>> getProfessorCountBySubject() {
         List<Object[]> results = subjectRepository.findProfessorCountBySubject();
